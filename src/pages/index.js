c79123aff09d59424925d300fd2676c89b62b82a
Greg McKelvey <mckelveygreg@gmail.com>
2020-09-12 16:24:59 -0700

    add link to resources

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/resources/">Go to "resources"</Link>
   </Layout>
 )
 
