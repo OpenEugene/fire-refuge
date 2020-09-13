375418db69361d9fcf412fed06ed234e93f14f2d
Greg McKelvey <mckelveygreg@gmail.com>
2020-09-12 17:12:07 -0700

    replace gatsby wording with Fire Refuge

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config()
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter",
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "Fire Refuge",
+    description: `Helping folks find temporary refuge from fire impacted zones`,
+    author: `@mckelveygreg`,
   },
   plugins: [
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,11 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Welcome</h1>
+    <p>We hope you are able to find the resources your need</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    <br />
     <Link to="/resources/">Go to "resources"</Link>
     <br />
     <Link to="/mapView/">Go to "Map View"</Link>
