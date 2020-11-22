e43e40f13bdff645e4fce66f90ba8e420d309159
Mark Davis <github@nohorse.com>
2020-11-21 16:07:46 -0800

    deployment details

--- FireRefuge/Client/bin/Debug/net5.0/wwwroot/service-worker.js
+++ FireRefuge/Client/bin/Debug/net5.0/wwwroot/service-worker.js
@@ -2,8 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-<<<<<<< HEAD
-/* Manifest version: rXZN5bpq */
-=======
-/* Manifest version: 33aiIzPa */
->>>>>>> 57e1840fcde2d6e6de58dd3fe5ea128e2a2bea67
+/* Manifest version: 4mOQ6hqo */
